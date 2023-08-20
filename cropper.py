d3f62c068c192a02d65aaaaf580c9665a6e739d1
George <gogent01@gmail.com>
2023-08-20 10:28:41 +0200

    Added command line arguments parsing

--- cropper.py
+++ cropper.py
@@ -1,44 +1,90 @@
-import glob
-import pathlib
+from glob import glob
+from pathlib import Path
+import argparse
 import subprocess
 import cv2
 
+def parseArguments():
+    script_directory = Path(__file__).parent.absolute()
+    parser = argparse.ArgumentParser(description = 'A tool to crop excessive white background around objects on images (or to grow some white background around if not enough).')
+    parser.add_argument(
+        '-i', 
+        '--input-dir',
+        action = 'store',
+        dest = 'input_dir', 
+        default = Path(script_directory / 'input').resolve(),
+        help = 'A path to a directory with images to crop.'
+    )
+    parser.add_argument(
+        '-o', 
+        '--output-dir',
+        action = 'store',
+        dest = 'output_dir', 
+        default = Path(script_directory / 'output').resolve(),
+        help = 'A path to an output directory to place cropped images.'
+    )
+    parser.add_argument(
+        '-p', 
+        '--padding',
+        action = 'store',
+        dest = 'padding_px', 
+        type = int,
+        default = 10,
+        help = 'A width of a white background padding around an object on an image in pixels.'
+    )
+    parser.add_argument(
+        '-e', 
+        '--extensions',
+        action = 'append',
+        dest = 'extensions',
+        type = str,
+        nargs = '*',
+        choices = ['png', 'jpg', 'tiff'],
+        default = ['png', 'jpg', 'tiff'],
+        help = 'Extensions of images to crop.'
+    )
 
-min_padding_px = 10
-input_extension = '.png'
-output_extension = '.png'
+    args = parser.parse_args()
 
-current_path = pathlib.Path(__file__).parent.absolute()
-load_path = str(current_path) + '/input/*{}'.format(input_extension)
-save_path = str(current_path) + '/output/'
+    if 'jpg' in args.extensions:
+        args.extensions += ['jpeg', 'jpe']
+
+    if 'tiff' in args.extensions:
+        args.extensions += ['tif']
+
+    return args
 
 
 def main():
+    args = parseArguments()
     kernel = cv2.getStructuringElement(cv2.MORPH_RECT,(2,2))
-    files = sorted(glob.glob(load_path, recursive = True))
+
+    files = []
+    for extension in args.extensions:
+        files += glob('{}/*.{}'.format(args.input_dir, extension), recursive = True)
 
     for file in files:
         # Load the image in black and white (0 - b/w, 1 - color).
         img_bw = cv2.imread(file, 0)
         img_col = cv2.imread(file, 1)
 
-        # Make <min_padding_px> border around both images.
+        # Make <args.padding_px> border around both images.
         img_bw = cv2.copyMakeBorder(
             img_bw,
-            top = min_padding_px,
-            bottom = min_padding_px,
-            left = min_padding_px,
-            right = min_padding_px,
+            top = args.padding_px,
+            bottom = args.padding_px,
+            left = args.padding_px,
+            right = args.padding_px,
             borderType = cv2.BORDER_CONSTANT,
             value = [255, 255, 255]
         )
 
         img_col = cv2.copyMakeBorder(
             img_col,
-            top = min_padding_px,
-            bottom = min_padding_px,
-            left = min_padding_px,
-            right = min_padding_px,
+            top = args.padding_px,
+            bottom = args.padding_px,
+            left = args.padding_px,
+            right = args.padding_px,
             borderType = cv2.BORDER_CONSTANT,
             value = [255, 255, 255]
         )
@@ -59,17 +105,17 @@ def main():
         rects = [cv2.boundingRect(cnt) for cnt in contours]
 
         # Calculate the combined bounding rectangle points.
-        top_x = min([x for (x, y, w, h) in rects]) - min_padding_px
-        top_y = min([y for (x, y, w, h) in rects]) - min_padding_px
-        bottom_x = max([x + w for (x, y, w, h) in rects]) + min_padding_px
-        bottom_y = max([y + h for (x, y, w, h) in rects]) + min_padding_px
+        top_x = min([x for (x, y, w, h) in rects]) - args.padding_px
+        top_y = min([y for (x, y, w, h) in rects]) - args.padding_px
+        bottom_x = max([x + w for (x, y, w, h) in rects]) + args.padding_px
+        bottom_y = max([y + h for (x, y, w, h) in rects]) + args.padding_px
 
         # Crop the colored image by the rectangle
         output = img_col[top_y:bottom_y, top_x:bottom_x]
-        filename = pathlib.Path(file).stem
+        filename = Path(file).name
 
-        # Save it as out.jpg
-        cv2.imwrite(save_path + filename + output_extension, output)
+        # Save the image with same name and extension
+        cv2.imwrite(str(args.output_dir / filename), output)
 
     print('Done!')
 
